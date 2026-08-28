85bb45fbc38598618522b5ac9bdd04add349f33d
Ho Khac Huy <huy@viennam.com>
2026-08-28 19:27:15 +0700

    add AddTemplateSectionVisibility migration

---
+++ Migrations/20260828112148_AddTemplateSectionVisibility.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VCMS.Landing.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTemplateSectionVisibility : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEnabled",
+                table: "TemplateSections",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEnabled",
+                table: "TemplateSections");
+        }
+    }
+}
