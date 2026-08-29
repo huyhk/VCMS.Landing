70b6efc2fbd602f78fabb779c62655a37a4c4445
huyhk <huy@viennam.com>
2026-08-29 09:51:05 +0700

    Bundle template styles with WebOptimizer

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -50,8 +50,6 @@ public class HomeController(ApplicationDbContext db, IContactEmailSender emailSe
             });
         }
         var renderedKeys = sections.Select(x => x.SectionKey).ToHashSet(StringComparer.Ordinal);
-        ViewData["HasHeroLeadForm"] = sections.Any(x =>
-            x.SectionType == "Hero" && x.LayoutVariant == "lead-form-right");
         var navigationItems = slots
             .Where(x => renderedKeys.Contains(x.SectionKey) && x.ShowInNavigation && sectionSchemas.GetNavigation(x.SectionDefinition.SchemaJson).Allowed)
             .Select(x => new NavigationItem(x.SectionKey,
